de5f3acffc93db4bbb22433ac90ed83740f98048
Martin Bachmann <martinpezbach@gmail.com>
2023-01-28 19:09:54 -0300

    feat: implement big card component

---
+++ src/components/BigCard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './BigCard.css';
+
+function BigCard({ post }) {
+  const { author, content, image, title } = post;
+  return (
+    <div className="big-card">
+      <img src={ image } alt={ title } />
+      <div className="content-inner">
+        <h2>{ author.name }</h2>
+        <h1>{ title }</h1>
+        <p>{ content }</p>
+      </div>
+    </div>
+  );
+}
+
+BigCard.propTypes = {
+  post: PropTypes.shape({
+    author: PropTypes.string,
+    content: PropTypes.string,
+    image: PropTypes.string,
+    title: PropTypes.string,
+  }),
+}.isRequired;
+
+export default BigCard;
