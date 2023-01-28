7f1276d1aea82ed8ab474d184b94f24fe7333580
Martin Bachmann <martinpezbach@gmail.com>
2023-01-28 12:58:23 -0300

    feat: create infinite scroll page structure

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import InfiniteScrollPage from './pages/InfiniteScrollPage';
 import NotFound from './pages/NotFound';
 
 function Routes() {
   return (
     <Switch>
+      <Route exact path="/" component={ InfiniteScrollPage } />
       <Route path="*" component={ NotFound } />
     </Switch>
   );
---
+++ src/pages/InfiniteScrollPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function InfiniteScrollPage() {
+  return (
+    <div>InfiniteScrollPage</div>
+  );
+}
+
+export default InfiniteScrollPage;
